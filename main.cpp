aa1c37850240cb7728cd465930c3154686bfd207
gthanksg <alamovazamjon@gmail.com>
2023-09-20 01:48:13 +0300

    todd howard moving head

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
 #include <Windows.h>
 
-// Step 1: Declare the Window procedure
+
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
-// Step 2: Declare the entry point of the application
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
-    // Step 3: Register the window class
+  
     const char* CLASS_NAME = "MyWindowClass";
 
     WNDCLASS wc = {};
@@ -16,33 +16,34 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
     RegisterClass(&wc);
 
-    // Step 4: Create the window
+  
     HWND hwnd = CreateWindowEx(
-            0,                              // Optional window styles
-            CLASS_NAME,                     // Window class name
-            "Movable Rectangle",            // Window title
-            WS_POPUP,                       // Window style
+            0,                            
+            CLASS_NAME,                   
+            "Transparent Image",          
+            WS_POPUP,                     
 
-            // Size and position
+          
             0, 0, GetSystemMetrics(SM_CXSCREEN), GetSystemMetrics(SM_CYSCREEN),
 
-            NULL,       // Parent window
-            NULL,       // Menu
-            hInstance,  // Instance handle
-            NULL        // Additional application data
+            nullptr,
+            nullptr,
+            hInstance,
+            nullptr
     );
 
-    if (hwnd == NULL)
+    if (hwnd ==
+    nullptr)
     {
         return 0;
     }
 
-    // Step 5: Show the window
+  
     ShowWindow(hwnd, nCmdShow);
 
-    // Step 6: Run the message loop
+  
     MSG msg = {};
-    while (GetMessage(&msg, NULL, 0, 0))
+    while (GetMessage(&msg, nullptr, 0, 0))
     {
         TranslateMessage(&msg);
         DispatchMessage(&msg);
@@ -51,27 +52,51 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     return 0;
 }
 
-// Step 7: Implement the Window procedure
+
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
-    static RECT rect = { 100, 100, 400, 400 }; // Initial rectangle position
+    static HBITMAP hBitmap = nullptr;
+    static RECT rect = { 100, 100, 400, 400 }; 
 
     switch (uMsg)
     {
+        case WM_CREATE:
+        {
+          
+            hBitmap = (HBITMAP)LoadImage(nullptr, "todd.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE | LR_CREATEDIBSECTION | LR_DEFAULTSIZE);
+            if (hBitmap == nullptr)
+            {
+                MessageBox(hwnd, "Failed to load the image.", "Error", MB_OK | MB_ICONERROR);
+                return -1;
+            }
+            return 0;
+        }
+
         case WM_PAINT:
         {
             PAINTSTRUCT ps;
             HDC hdc = BeginPaint(hwnd, &ps);
 
-            // Clear the window with a white background
+          
             HBRUSH hBackgroundBrush = CreateSolidBrush(RGB(255, 255, 255));
             FillRect(hdc, &ps.rcPaint, hBackgroundBrush);
             DeleteObject(hBackgroundBrush);
 
-            // Draw the rectangle
-            HBRUSH hBrush = CreateSolidBrush(RGB(255, 0, 0));
-            FillRect(hdc, &rect, hBrush);
-            DeleteObject(hBrush);
+          
+            HDC hdcMem = CreateCompatibleDC(hdc);
+            auto hBitmapOld = (HBITMAP)SelectObject(hdcMem, hBitmap);
+
+            BITMAP bm;
+            GetObject(hBitmap, sizeof(bm), &bm);
+            int x = rect.left;
+            int y = rect.top;
+
+            COLORREF transparentColor = RGB(255, 0, 255); 
+
+            TransparentBlt(hdc, x, y, bm.bmWidth, bm.bmHeight, hdcMem, 0, 0, bm.bmWidth, bm.bmHeight, transparentColor);
+
+            SelectObject(hdcMem, hBitmapOld);
+            DeleteDC(hdcMem);
 
             EndPaint(hwnd, &ps);
             return 0;
@@ -82,32 +107,30 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
             int xPos = rect.left;
             int yPos = rect.top;
             int delta = GET_WHEEL_DELTA_WPARAM(wParam);
-
-            // Check if Shift key is being held down
             if (GetKeyState(VK_SHIFT) & 0x8000)
             {
-                // Shift+Scroll: Change position in the y-axis
                 yPos -= delta;
             }
             else
             {
-                // Scroll: Change size in the x-axis
                 int width = rect.right - rect.left;
                 width += delta;
                 if (width < 0)
                     width = 0;
                 xPos = rect.left + (rect.right - rect.left - width) / 2;
             }
-
-            // Update the rectangle position and size
             SetRect(&rect, xPos, yPos, xPos + (rect.right - rect.left), yPos + (rect.bottom - rect.top));
 
-            // Trigger a repaint to update the window
-            InvalidateRect(hwnd, NULL, TRUE);
+          
+            InvalidateRect(hwnd, nullptr, TRUE);
             return 0;
         }
 
         case WM_DESTROY:
+            if (hBitmap != nullptr)
+            {
+                DeleteObject(hBitmap);
+            }
             PostQuitMessage(0);
             return 0;
     }
