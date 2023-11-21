10ee75e6148ac41b239eb9e75b828f25765f11b8
ewsc <alamovazamjon@gmail.com>
2023-11-21 17:45:41 +0300

    static dll load added

---
+++ LabTres/StaticLoad.cpp
@@ -0,0 +1,11 @@
+// StaticLoad.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#include <iostream>
+#include "Dll1.h"
+
+int main()
+{
+    SearchAndReplaceString();
+    std::cout << "Dll successfully loaded." << std::endl;
+}
+
